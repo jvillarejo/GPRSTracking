20bf022ba4eee73fdd2135f3006db490cff6e8de
jvillarejo <arzivian87@gmail.com>
2012-09-09 19:10:57 -0300

    GPRS Tracking armado esqueleto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GPRSTRacking::Application.config.secret_token = '5b3f857c361e58cd886ef044d7f9a0973c78381023fbcf796c8bf88a3b8ff038187b3f88f1ff79070e763d953a8eccf55ba9a3401838b5feb1c130856639a2b9'
